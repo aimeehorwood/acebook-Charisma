d4dc8ba04cdee69da147216c4e669473699ecb48
Aimee <aimeehorwood@hotmail.com>
2023-01-19 10:23:02 +0000

    removes duplicate button in my profile page

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -1,83 +1,143 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const SignUpForm = ({ navigate }) => {
-  
   const [error, setError] = useState(null);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [aboutMe, setAboutMe] = useState("");
-  const [image, setImage] = useState('https://www.shareicon.net/data/128x128/2016/11/28/857788_animal_512x512.png')
-
+  const [image, setImage] = useState(
+    "https://www.shareicon.net/data/128x128/2016/11/28/857788_animal_512x512.png"
+  );
 
   const handleImageChange = (event) => {
-    setImage(event.target.value)
-  }
+    setImage(event.target.value);
+  };
 
   const handleNameChange = (event) => {
-    setName(event.target.value)
-  }
-  
+    setName(event.target.value);
+  };
+
   const handleAboutMeChange = (event) => {
-    setAboutMe(event.target.value)
-  }
-  
+    setAboutMe(event.target.value);
+  };
+
   const handleEmailChange = (event) => {
-    setEmail(event.target.value)
-  }
+    setEmail(event.target.value);
+  };
 
   const handlePasswordChange = (event) => {
-    setPassword(event.target.value)
-  }
+    setPassword(event.target.value);
+  };
 
   const handleSubmit = async (event) => {
-    setError(null)
+    setError(null);
     event.preventDefault();
 
-    const response = await fetch( '/users/signup', {
-      method: 'post',
+    const response = await fetch("/users/signup", {
+      method: "post",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name: name, email: email, password: password, aboutMe: aboutMe, friends: [], image: image })
-    })
-    const data = await response.json()
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+        aboutMe: aboutMe,
+        friends: [],
+        image: image,
+      }),
+    });
+    const data = await response.json();
     if (response.status === 201) {
-      navigate('/login')
-      } else {
-      setError(data.error)
-      navigate('/signup')
+      navigate("/login");
+    } else {
+      setError(data.error);
+      navigate("/signup");
     }
-  }
+  };
 
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        required
+        placeholder="Name"
+        id="name"
+        type="text"
+        value={name}
+        onChange={handleNameChange}
+      />
+      <input
+        required
+        placeholder="About me"
+        id="aboutMe"
+        type="text"
+        value={aboutMe}
+        onChange={handleAboutMeChange}
+      />
+      <input
+        placeholder="Email"
+        id="email"
+        type="text"
+        value={email}
+        onChange={handleEmailChange}
+      />
+      <input
+        placeholder="Password"
+        id="password"
+        type="password"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <input id="submit" type="submit" value="Submit" />
+      {error && <div className="error">{error}</div>}
+      <div>
+        <h5> Select a Profile Picture for your Profile </h5>
+        <img
+          src={
+            "https://www.shareicon.net/data/128x128/2016/11/28/857788_animal_512x512.png"
+          }
+          alt="profile"
+        />
+        <img
+          src={
+            "https://www.shareicon.net/data/128x128/2016/11/28/857792_animal_512x512.png"
+          }
+          alt="profile"
+        />
+        <img
+          src={
+            "https://www.shareicon.net/data/128x128/2016/12/20/863853_snake_512x512.png"
+          }
+          alt="profile"
+        />
+        <img
+          src={
+            "https://www.shareicon.net/data/128x128/2017/01/06/868266_bug_512x512.png"
+          }
+          alt="profile"
+        />
 
-    return (
-      <form onSubmit={handleSubmit}>
-        <input required placeholder='Name' id="name" type='text' value={ name } onChange={handleNameChange} />
-        <input required placeholder='About me' id="aboutMe" type='text' value={ aboutMe } onChange={handleAboutMeChange} />
-        <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
-        <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
-        <input id='submit' type="submit" value="Submit" />
-        {error && <div className="error">{error}</div>}
-        <div> 
-          <h5> Select a Profile Picture for your Profile </h5>
-        <img src={'https://www.shareicon.net/data/128x128/2016/11/28/857788_animal_512x512.png'} alt = "profile" />
-        <img src={'https://www.shareicon.net/data/128x128/2016/11/28/857792_animal_512x512.png'} alt = "profile" />
-        <img src={'https://www.shareicon.net/data/128x128/2016/12/20/863853_snake_512x512.png'} alt = "profile" />
-        <img src={'https://www.shareicon.net/data/128x128/2017/01/06/868266_bug_512x512.png'} alt = "profile" />
-        
         {/* <button id="profile" onClick={handlePictureChange}> Select me </button> */}
         <select id="selectList" onChange={handleImageChange}>
-          <option value='https://www.shareicon.net/data/128x128/2016/11/28/857788_animal_512x512.png'>Option 1</option>
-          <option value='https://www.shareicon.net/data/128x128/2016/11/28/857792_animal_512x512.png'>Option 2</option>
-          <option value='https://www.shareicon.net/data/128x128/2016/12/20/863853_snake_512x512.png'>Option 3</option>
-          <option value='https://www.shareicon.net/data/128x128/2017/01/06/868266_bug_512x512.png'>Option 4</option>
-      </select>
-        
-        </div>
-      </form>
-
-    );
-}
+           {" "}
+          <option value="https://www.shareicon.net/data/128x128/2016/11/28/857788_animal_512x512.png">
+            Option 1
+          </option>
+           {" "}
+          <option value="https://www.shareicon.net/data/128x128/2016/11/28/857792_animal_512x512.png">
+            Option 2
+          </option>
+          <option value="https://www.shareicon.net/data/128x128/2016/12/20/863853_snake_512x512.png">
+            Option 3
+          </option>
+          <option value="https://www.shareicon.net/data/128x128/2017/01/06/868266_bug_512x512.png">
+            Option 4
+          </option>
+        </select>
+      </div>
+    </form>
+  );
+};
 
 export default SignUpForm;
--- frontend/src/components/userProfileDetails/UserProfileDetails.js
+++ frontend/src/components/userProfileDetails/UserProfileDetails.js
@@ -3,30 +3,25 @@ import { Link } from "react-router-dom";
 import "./UserProfileDetails.css";
 import CreatePost from "../createPost/CreatePost";
 import Post from "../post/Post";
-import FriendsList from "../friendsList/FriendsList"
-import { useParams } from 'react-router-dom';
-
-
+import FriendsList from "../friendsList/FriendsList";
+import { useParams } from "react-router-dom";
 
 const Profile = () => {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
   const [posts, setPosts] = useState(null);
-  const [updated, setUpdated] = useState(null)
+  const [updated, setUpdated] = useState(null);
 
-  const [friendsView, setFriendsView] = useState(false)
-  const [myProfilePage, setMyProfilePage] = useState(null)
-  const [friendshipStatus, setFriendshipStatus] = useState(null)
-  const [friendshipRequestStatus, setFriendshipRequestStatus] = useState(null)
-  const [friends, setFriends] = useState(null)
-  const [friendRequests, setFriendRequests] = useState(null)
+  const [friendsView, setFriendsView] = useState(false);
+  const [myProfilePage, setMyProfilePage] = useState(null);
+  const [friendshipStatus, setFriendshipStatus] = useState(null);
+  const [friendshipRequestStatus, setFriendshipRequestStatus] = useState(null);
+  const [friends, setFriends] = useState(null);
+  const [friendRequests, setFriendRequests] = useState(null);
   const [image, setImageUrl] = useState("");
-
-
 
   let { id } = useParams();
 
-  
   useEffect(() => {
     if (token) {
       fetch(`/users/${id}`)
@@ -35,17 +30,22 @@ const Profile = () => {
           setToken(window.localStorage.getItem("token"));
           setUser(data.user);
 
-          setMyProfilePage(id===window.localStorage.getItem("user_id"))
-          await setFriendshipRequestStatus(data.user.friendRequests.includes(window.localStorage.getItem("user_id")))
-          await setFriendshipStatus(data.user.friends.includes(window.localStorage.getItem("user_id")))
-        
-          await setFriends(data.user.friends)
-          await setFriendRequests(data.user.friendRequests)
-          setImageUrl(data.user.image);
+          setMyProfilePage(id === window.localStorage.getItem("user_id"));
+          await setFriendshipRequestStatus(
+            data.user.friendRequests.includes(
+              window.localStorage.getItem("user_id")
+            )
+          );
+          await setFriendshipStatus(
+            data.user.friends.includes(window.localStorage.getItem("user_id"))
+          );
 
+          await setFriends(data.user.friends);
+          await setFriendRequests(data.user.friendRequests);
+          setImageUrl(data.user.image);
         });
     }
-  }, [id,updated]);
+  }, [id, updated]);
 
   useEffect(() => {
     if (token) {
@@ -59,78 +59,85 @@ const Profile = () => {
           window.localStorage.setItem("token", data.token);
           setToken(window.localStorage.getItem("token"));
           await setPosts(data.posts);
-          setUpdated(false)
+          setUpdated(false);
         });
     }
-  }, [updated,id,friendsView,friendshipStatus]);
+  }, [updated, id, friendsView, friendshipStatus]);
 
   const viewFriends = () => {
-    setFriendsView(!friendsView)
-    setUpdated(false)
-  }
+    setFriendsView(!friendsView);
+    setUpdated(false);
+  };
 
-  const handleFriendRequest = (event,requester_id) => {
-    event.preventDefault()
+  const handleFriendRequest = (event, requester_id) => {
+    event.preventDefault();
     if (token) {
       fetch(`http://localhost:3000/users/${requester_id}`, {
-        method: 'PATCH',
+        method: "PATCH",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
-          field: 'request',
-          requester: window.localStorage.getItem('user_id'),
+          field: "request",
+          requester: window.localStorage.getItem("user_id"),
         }),
       }).then((response) => {
-        setFriendshipRequestStatus(true)
-      })
+        setFriendshipRequestStatus(true);
+      });
     }
-  }
+  };
 
   return (
     <>
-    <div className="profile-details">
-      <h1>My details</h1>
-      <p>Name: {user && user.name}</p>
-      <p>About me: {user && user.aboutMe}</p>
+      <div className="profile-details">
+        <h1>My details</h1>
+        <p>Name: {user && user.name}</p>
+        <p>About me: {user && user.aboutMe}</p>
 
-      {myProfilePage && <button onClick={viewFriends}>Friends List</button>}
+        {myProfilePage && <button onClick={viewFriends}>Friends List</button>}
 
-      <img src={image} id="profile-pics" alt="img" />
-      <button>Friends List</button>
-
-    </div>
-    {(!myProfilePage && friendshipStatus) && (
-      `You and ${user.name} are friends`
-    )}
-    {(!myProfilePage && friendshipRequestStatus) && (
-      `You and have requested to be friends with ${user.name}`
-    )}
-    {(!myProfilePage && (!friendshipRequestStatus && !friendshipStatus)) && (
-      <button onClick={(event) => handleFriendRequest(event,user._id)}>Send friend request</button>
-    )}
-      
-    
-    
-
-    {friendsView && <FriendsList userFriends={friends} userFriendRequests={friendRequests} updated={updated} setUpdated={setUpdated} currentUser={user} />}
-    {!friendsView && 
-      <div className="user-posts">
-      <h2 id="post" className="feedHeader">My Posts</h2>
-      {myProfilePage && <CreatePost setUpdated={setUpdated}/>}
-            <div id='feed' role="feed">
-              {posts && posts.sort(function(postA, postB) {
-                return (new Date(postB.createdAt) - new Date(postA.createdAt));
-              }).map(
-                (post) => ( <Post setUpdated={setUpdated} post={ post } key={ post._id } /> )
-              )}
-          </div>
+        <img src={image} id="profile-pics" alt="img" />
       </div>
-}
-      </>
+      {!myProfilePage && friendshipStatus && `You and ${user.name} are friends`}
+      {!myProfilePage &&
+        friendshipRequestStatus &&
+        `You and have requested to be friends with ${user.name}`}
+      {!myProfilePage && !friendshipRequestStatus && !friendshipStatus && (
+        <button onClick={(event) => handleFriendRequest(event, user._id)}>
+          Send friend request
+        </button>
+      )}
+
+      {friendsView && (
+        <FriendsList
+          userFriends={friends}
+          userFriendRequests={friendRequests}
+          updated={updated}
+          setUpdated={setUpdated}
+          currentUser={user}
+        />
+      )}
+      {!friendsView && (
+        <div className="user-posts">
+          <h2 id="post" className="feedHeader">
+            My Posts
+          </h2>
+          {myProfilePage && <CreatePost setUpdated={setUpdated} />}
+          <div id="feed" role="feed">
+            {posts &&
+              posts
+                .sort(function (postA, postB) {
+                  return new Date(postB.createdAt) - new Date(postA.createdAt);
+                })
+                .map((post) => (
+                  <Post setUpdated={setUpdated} post={post} key={post._id} />
+                ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
 export default Profile;
-
